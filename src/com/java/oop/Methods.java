cf5d734ddc5da7f22321f24cf7bc7a3b264beb3e
anis <anis.siam@hotmail.com>
2022-09-07 19:01:49 +0300

    7/9/2022 -Multi dim.. array -OOP -Methods

--- src/com/java/oop/Methods.java
+++ src/com/java/oop/Methods.java
@@ -1,4 +1,54 @@
 package com.java.oop;
 
+import java.awt.*;
+import java.util.Scanner;
+
 public class Methods {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        //System.out.println(addNumbers2(x, y));
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first no");
+        int z = scanner.nextInt();
+        System.out.println("Enter sec.. no");
+        int f = scanner.nextInt();
+        addNumbers1(z,f);*/
+
+        concat("anis"," siam");
+        String a= concat1("anis", "siam");
+        System.out.println(concat1("anis", "siam"));
+    }
+
+
+    public static void concat(String st1 , String st2){
+        System.out.println(st1 + st2);
+    }
+
+    public static String concat1(String st1 , String st2){
+        String co = st1 + st2;
+        return co;
+    }
+    public static void addNumbers(){
+        int x = 5;
+        int y = 10;
+        System.out.println(x + y);
+
+
+    }
+
+    public static void addNumbers1(int x , int y){
+
+        System.out.println(x + y);
+    }
+    public static int addNumbers2(int x , int y){
+
+        return (x+y);
+    }
+    public static void sub(){
+        int x = 5;
+        int y = 10;
+        System.out.println(x -y);
+    }
 }
